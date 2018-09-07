c896535c81f837f637344ff5e8f38fefa21ee160
Oscar Mattsson <oscar_mattsson@live.se>
2018-09-07 13:58:52 +0200

    Added custom attribute matcher for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,3 +52,23 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
+
+RSpec::Matchers.define :have_attr_accessor do |field|
+  match do |object_instance|
+    object_instance.respond_to?(field) &&
+      object_instance.respond_to?("#{field}=")
+  end
+
+  failure_message do |object_instance|
+    "expected attr_accessor for #{field} on #{object_instance}"
+  end
+
+  failure_message_when_negated do |object_instance|
+    "expected attr_accessor for #{field} not to be defined on \
+    #{object_instance}"
+  end
+
+  description do
+    "have an attr_accessor `#{field}`"
+  end
+end
